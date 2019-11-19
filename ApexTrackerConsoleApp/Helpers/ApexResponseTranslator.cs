e35ec1efaa292b975511a1e269e7b3777bd3faa3
Filiph Eriksson Falk <filiph.erikssonfalk@hiq.se>
2019-11-19 22:08:49 +0100

    Added Season 3 Wins Tracker ID:s to translator

--- ApexTrackerConsoleApp/Helpers/ApexResponseTranslator.cs
+++ ApexTrackerConsoleApp/Helpers/ApexResponseTranslator.cs
@@ -30,52 +30,52 @@ namespace ApexTrackerConsoleApp.Helpers
             //Caustic
             ApexResponseDict.Add(new KeyValuePair<string, string>("15753331", "Kills"));
             ApexResponseDict.Add(new KeyValuePair<string, string>("1039353772", "Top3"));
-            ApexResponseDict.Add(new KeyValuePair<string, string>("628023316", "Wins"));
+            ApexResponseDict.Add(new KeyValuePair<string, string>("1846562893", "Wins"));
 
             //Bangalore
             ApexResponseDict.Add(new KeyValuePair<string, string>("1814522143", "Kills"));
             ApexResponseDict.Add(new KeyValuePair<string, string>("486927953", "Top3"));
-            ApexResponseDict.Add(new KeyValuePair<string, string>("1872592848", "Wins"));
+            ApexResponseDict.Add(new KeyValuePair<string, string>("876803968", "Wins"));
             
             //Bloodhound
             ApexResponseDict.Add(new KeyValuePair<string, string>("1049917798", "Kills"));
             ApexResponseDict.Add(new KeyValuePair<string, string>("59904350", "Top3"));
-            ApexResponseDict.Add(new KeyValuePair<string, string>("1413070446", "Wins"));
+            ApexResponseDict.Add(new KeyValuePair<string, string>("1414389154", "Wins"));
 
             //Gibraltar
             ApexResponseDict.Add(new KeyValuePair<string, string>("345008354", "Kills"));
             ApexResponseDict.Add(new KeyValuePair<string, string>("299529402", "Top3"));
-            ApexResponseDict.Add(new KeyValuePair<string, string>("1082828160", "Wins"));
+            ApexResponseDict.Add(new KeyValuePair<string, string>("1648116726", "Wins"));
             
             //Lifeline
             ApexResponseDict.Add(new KeyValuePair<string, string>("1509839340", "Kills"));
             ApexResponseDict.Add(new KeyValuePair<string, string>("692891435", "Top3"));
-            ApexResponseDict.Add(new KeyValuePair<string, string>("1740545206", "Wins"));
+            ApexResponseDict.Add(new KeyValuePair<string, string>("1143220112", "Wins"));
             
             //Mirage
             ApexResponseDict.Add(new KeyValuePair<string, string>("1730527550", "Kills"));
             ApexResponseDict.Add(new KeyValuePair<string, string>("1996756430", "Top3"));
-            ApexResponseDict.Add(new KeyValuePair<string, string>("1500066309", "Wins"));
+            ApexResponseDict.Add(new KeyValuePair<string, string>("1136168729", "Wins"));
 
             //Pathfinder
             ApexResponseDict.Add(new KeyValuePair<string, string>("196161681", "Kills"));
             ApexResponseDict.Add(new KeyValuePair<string, string>("704679779", "Top3"));
-            ApexResponseDict.Add(new KeyValuePair<string, string>("1358938113", "Wins"));
+            ApexResponseDict.Add(new KeyValuePair<string, string>("1321127947", "Wins"));
             
             //Wraith
             ApexResponseDict.Add(new KeyValuePair<string, string>("1618935778", "Kills"));
             ApexResponseDict.Add(new KeyValuePair<string, string>("1974771594", "Top3"));
-            ApexResponseDict.Add(new KeyValuePair<string, string>("2023347527", "Wins"));
+            ApexResponseDict.Add(new KeyValuePair<string, string>("508273158", "Wins"));
             
             //Octane
             ApexResponseDict.Add(new KeyValuePair<string, string>("303788636", "Kills"));
             ApexResponseDict.Add(new KeyValuePair<string, string>("1853728991", "Top3"));
-            ApexResponseDict.Add(new KeyValuePair<string, string>("290334183", "Wins"));
+            ApexResponseDict.Add(new KeyValuePair<string, string>("1249249170", "Wins"));
 
             //Wattson
             ApexResponseDict.Add(new KeyValuePair<string, string>("1449585426", "Kills"));
             ApexResponseDict.Add(new KeyValuePair<string, string>("1013733932", "Top3"));
-            ApexResponseDict.Add(new KeyValuePair<string, string>("706925895", "Wins"));
+            ApexResponseDict.Add(new KeyValuePair<string, string>("153803806", "Wins"));
 
             //Crypto
             ApexResponseDict.Add(new KeyValuePair<string, string>("1154431005", "Kills"));
